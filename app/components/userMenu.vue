619d49cbdfc62e9b9d5ed6e79db7bc22115c3a74
Le-Andris Daniel <ldaniel38@gmail.com>
2026-09-07 03:26:38 -0500

    remove theme selection menu

--- app/components/userMenu.vue
+++ app/components/userMenu.vue
@@ -8,27 +8,6 @@ defineProps<{
 }>()
 
 const colorMode = useColorMode()
-const appConfig = useAppConfig()
-
-const colors = [
-  'red',
-  'orange',
-  'amber',
-  'yellow',
-  'lime',
-  'green',
-  'emerald',
-  'teal',
-  'cyan',
-  'sky',
-  'blue',
-  'indigo',
-  'violet',
-  'purple',
-  'fuchsia',
-  'pink',
-  'rose'
-]
 
 const items = computed<DropdownMenuItem[][]>(() => [
   [
@@ -54,22 +33,6 @@ const items = computed<DropdownMenuItem[][]>(() => [
     }
   ],
   [
-    {
-      label: 'Theme',
-      icon: 'i-lucide-palette',
-      children: colors.map((color) => ({
-        label: color,
-        chip: color,
-        slot: 'chip',
-        checked: appConfig.ui.colors.primary === color,
-        type: 'checkbox',
-        onSelect: (e) => {
-          e.preventDefault()
-
-          appConfig.ui.colors.primary = color
-        }
-      }))
-    },
     {
       label: 'Appearance',
       icon: 'i-lucide-sun-moon',
@@ -136,17 +99,5 @@ const items = computed<DropdownMenuItem[][]>(() => [
         trailingIcon: 'text-dimmed'
       }"
     />
-
-    <template #chip-leading="{ item }">
-      <div class="inline-flex items-center justify-center shrink-0 size-5">
-        <span
-          class="rounded-full ring ring-bg bg-(--chip-light) dark:bg-(--chip-dark) size-2"
-          :style="{
-            '--chip-light': `var(--color-${(item as any).chip}-500)`,
-            '--chip-dark': `var(--color-${(item as any).chip}-400)`
-          }"
-        />
-      </div>
-    </template>
   </UDropdownMenu>
 </template>
